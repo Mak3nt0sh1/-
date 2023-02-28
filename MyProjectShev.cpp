d4032b4425f0ed69c64d9b8422d37a59e5572607
Shevtsov_Konstantin <workoutk2019@gmail.com>
2023-02-28 23:48:59 +0800

    ver_3

--- MyProjectShev.cpp
+++ MyProjectShev.cpp
@@ -8,33 +8,105 @@ int main()
 {
 	srand(time(0));
 	rand();
-	system("mode con cols=51 lines=31"); 
-	MoveWindow(GetConsoleWindow(), 50, 50, 2000, 2000, true); 
+	system("mode con cols=51 lines=31");
+	MoveWindow(GetConsoleWindow(), 50, 50, 2000, 2000, true);
 
-	const int width = 50, height = 30; 
-	const int max_length = 50; 
+	const int width = 50, height = 30;
+	const int max_length = 50;
 	int array_X[max_length];
-	int array_Y[max_length]; 
-	int length = 1; 
-	array_X[0] = width / 2; 
-	array_Y[0] = height / 2; 
-	int dx = 1, dy = 0; 
-	int X_apple; 
-	int Y_apple; 
-	do 
+	int array_Y[max_length];
+	int length = 1;
+	array_X[0] = width / 2;
+	array_Y[0] = height / 2;
+	int dx = 1, dy = 0;
+	int X_apple;
+	int Y_apple;
+	do
 	{
 		X_apple = rand() % (width - 2) + 1;
 		Y_apple = rand() % (height - 2) + 1;
 	} while (X_apple != array_X[length - 1] && Y_apple != array_Y[length - 1]);
 
-	int sleep_time = 100; 
+	int sleep_time = 100;
 
-	char snake = '*'; 
-	char apple = 'o'; 
-	char head = 1; 
-	COORD c; 
-	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE); 
-	CONSOLE_CURSOR_INFO cci = { sizeof(cci), false }; 
-	SetConsoleCursorInfo(h, &cci); 
+	char snake = '*';
+	char apple = 'o';
+	char head = 1;
+	COORD c;
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cci = { sizeof(cci), false };
+	SetConsoleCursorInfo(h, &cci);
 
 	SetConsoleTextAttribute(h, 4);
+	for (int y = 0; y < height; y++)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			char s;
+			if (x == 0 && y == 0)
+				s = 218;
+			else if (x == 0 && y == height - 1)
+				s = 192;
+			else if (y == 0 && x == width - 1)
+				s = 191;
+			else if (y == height - 1 && x == width - 1)
+				s = 217;
+			else if (y == 0 || y == height - 1)
+				s = 196;
+			else if (x == 0 || x == width - 1)
+				s = 179;
+			else s = ' ';
+			putchar(s);
+		}
+		cout << endl;
+	}
+	c.X = X_apple;
+	c.Y = Y_apple;
+	SetConsoleCursorPosition(h, c);
+	SetConsoleTextAttribute(h, 12);
+	putchar(apple);
+
+	c.X = array_X[0];
+	c.Y = array_Y[0];
+	SetConsoleCursorPosition(h, c);
+	SetConsoleTextAttribute(h, 10);
+	putchar(head);
+
+	bool flag = true;
+	do
+	{
+		Sleep(sleep_time);
+
+		if (_kbhit())
+		{
+			int k = _getch();
+			if (k == 0 || k == 224)
+				k = _getch();
+			switch (k)
+			{
+			case 80:
+				dy = 1;
+				dx = 0;
+				break;
+			case 72:
+				dy = -1;
+				dx = 0;
+				break;
+			case 75:
+				dy = 0;
+				dx = -1;
+				break;
+			case 77:
+				dy = 0;
+				dx = 1;
+				break;
+			case 27:
+				flag = false;
+				break;
+			}
+		}
+	} while (flag);
+	system("cls");
+	cout << "GAME OVER\n";
+	system("pause");
+}
