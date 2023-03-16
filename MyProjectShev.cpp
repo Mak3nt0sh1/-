7c5edfe4a83dd8e80495051cf1af2d6ee54fd0c2
Shevtsov_Konstantin <workoutk2019@gmail.com>
2023-03-17 00:21:15 +0800

    ver6

--- MyProjectShev.cpp
+++ MyProjectShev.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-	srand(time(0));
+	srand(time(0)); // запуск генератора случайных чисел
 	rand();
 	system("mode con cols=51 lines=31");
 	MoveWindow(GetConsoleWindow(), 50, 50, 2000, 2000, true);
@@ -21,7 +21,7 @@ int main()
 	int dx = 1, dy = 0;
 	int X_apple;
 	int Y_apple;
-	do
+	do // цикл ставит координаты яблока случанйм образом - но чтобы они не совпадали со "змейкой"
 	{
 		X_apple = rand() % (width - 2) + 1;
 		Y_apple = rand() % (height - 2) + 1;
@@ -34,11 +34,11 @@ int main()
 	char head = 1;
 	COORD c;
 	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_CURSOR_INFO cci = { sizeof(cci), false };
+	CONSOLE_CURSOR_INFO cci = { sizeof(cci), false }; // создание параметров на отображение курсора
 	SetConsoleCursorInfo(h, &cci);
 
-	SetConsoleTextAttribute(h, 5);
-	for (int y = 0; y < height; y++)
+	SetConsoleTextAttribute(h, 5); // установка цвета, которым рисуется рамка поля
+	for (int y = 0; y < height; y++) // двойной цикл на отрисовку рамки
 	{
 		for (int x = 0; x < width; x++)
 		{
@@ -60,10 +60,10 @@ int main()
 		}
 		cout << endl;
 	}
-	c.X = X_apple;
+	c.X = X_apple; // связываем объект координат с позициями "яблока"
 	c.Y = Y_apple;
 	SetConsoleCursorPosition(h, c);
-	SetConsoleTextAttribute(h, 12);
+	SetConsoleTextAttribute(h, 12); // устанавливаем красный цвет для отрисовки "яблока"
 	putchar(apple);
 
 	c.X = array_X[0];
@@ -72,12 +72,12 @@ int main()
 	SetConsoleTextAttribute(h, 10);
 	putchar(head);
 
-	bool flag = true;
-	do
+	bool flag = true; // переменная для управления ходом цикла
+	do // цикл игры
 	{
 		Sleep(sleep_time);
 
-		if (_kbhit())
+		if (_kbhit()) // проверяем, была ли нажата какая-либо клавиша и запускаем её обработку в случае True
 		{
 			int k = _getch();
 			if (k == 0 || k == 224)
@@ -105,13 +105,13 @@ int main()
 				break;
 			}
 		}
-		int X = array_X[length - 1] + dx;
+		int X = array_X[length - 1] + dx; // определяем значение абсциссы головы "змейки" после смещения
 		int Y = array_Y[length - 1] + dy;
 		if (X == 0 || X == width - 1 || Y == 0 || Y == height - 1)
 		{
 			flag = false;
 		}
-		else if (X == X_apple && Y == Y_apple)
+		else if (X == X_apple && Y == Y_apple) // проверка на достижение "яблока"
 		{
 			c.X = array_X[length - 1];
 			c.Y = array_Y[length - 1];
@@ -124,7 +124,7 @@ int main()
 			SetConsoleCursorPosition(h, c);
 			putchar(head);
 
-			if (length == max_length)
+			if (length == max_length) // проверка, достигла ли длина "змейки" своего максимального значения
 			{
 				break;
 			}
@@ -146,14 +146,14 @@ int main()
 			putchar(apple);
 			SetConsoleTextAttribute(h, 10);
 		}
-		else 
+		else // случай, когда голова "змейки" оказалась на новой пустой позиции
 		{
 			c.X = array_X[0];
 			c.Y = array_Y[0];
 			SetConsoleCursorPosition(h, c); 
 			putchar(' '); 
 
-			if (length > 1) 
+			if (length > 1) // если длина змейки больше
 			{
 				c.X = array_X[length - 1]; 
 				c.Y = array_Y[length - 1];
@@ -161,19 +161,19 @@ int main()
 				putchar(snake); 
 			}
 
-			for (int i = 0; i < length - 1; i++) 
+			for (int i = 0; i < length - 1; i++) // запускаем цикл свдига координат звеньев "змейки"
 			{
 				array_X[i] = array_X[i + 1]; 
 				array_Y[i] = array_Y[i + 1];
 			}
 
-			c.X = array_X[length - 1] = X; 
+			c.X = array_X[length - 1] = X; // устанавливаем новую позицию головы "змейки"
 			c.Y = array_Y[length - 1] = Y;
 			SetConsoleCursorPosition(h, c); 
 			putchar(head); 
 		}
 	
-		} while (flag);
+		} while (flag); // выходим из цикла, если сброшена управляющая переменная
 		system("cls");
 		cout << "GAME OVER\n";
 		system("pause");
